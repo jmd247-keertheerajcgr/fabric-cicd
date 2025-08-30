dc1dfb8f04957a394cdfb948132dc60c271cbc22
jmd247-keertheerajcgr <147801908+jmd247-keertheerajcgr@users.noreply.github.com>
2025-08-31 01:57:17 +0530

    Update notebook-content.py

--- deploy_fabric.Notebook/notebook-content.py
+++ deploy_fabric.Notebook/notebook-content.py
@@ -1,24 +1,25 @@
 # Fabric notebook source
-
 # METADATA ********************
-
 # META {
 # META   "kernel_info": {
 # META     "name": "synapse_pyspark"
 # META   },
 # META   "dependencies": {}
 # META }
-
 # CELL ********************
-
 # Welcome to your new notebook
 # Type here in the cell editor to add code!
+
+import os
 from fabric_cicd import FabricWorkspace, publish_all_items, unpublish_all_orphan_items
+
+# Dynamically get current working directory for repository_directory (works in automation pipelines)
+repository_directory = os.getcwd()
 
 # Initialize the FabricWorkspace object with the required parameters
 target_workspace = FabricWorkspace(
     workspace_id = "d1ecda06-105b-4dcb-9af8-d14c17383377",
-    repository_directory = "C:/Users/KeertheerajCGR/Downloads/fabric-cicd/fabric-cicd",
+    repository_directory = repository_directory,
     item_type_in_scope = ["Notebook","DataPipeline","Environment"],
 )
 
@@ -29,7 +30,6 @@ publish_all_items(target_workspace)
 unpublish_all_orphan_items(target_workspace)
 
 # METADATA ********************
-
 # META {
 # META   "language": "python",
 # META   "language_group": "synapse_pyspark"
